4b9f38528517a621ba7703d862758deb86fcd603
nnmini <namini.niranjan@gmail.com>
2021-01-02 17:07:10 -0800

    calendarapp

--- calendar_app/src/components/CalendarEntry.vue
+++ calendar_app/src/components/CalendarEntry.vue
@@ -1,10 +1,11 @@
 <template>
      <div id="calendar-entry">
       <div class="calendar-entry-note">
-        <input type="text" placeholder="New Event" />
+        <input type="text" v-model="newEvent" placeholder="New Event" />
         <p class="calendar-entry-day">Day of event: <span class="bold">{{getActiveDayTitle}}</span></p>
-        <a class="button is-primary is-small is-outlined">Submit</a>
+        <a class="button is-primary is-small is-outlined" @click="submitEvent(newEvent)">Submit</a>
       </div>
+      <p v-if="error">You must type something first</p>
     </div>
 </template>
 
@@ -12,9 +13,30 @@
 import {store} from '../store'
 export default {
 name: "CalendarEntry",
+data(){
+  return {
+ newEvent:'',
+ error: false
+  }
+ 
+},
 computed: {
   getActiveDayTitle(){
     return store.getActiveDay().fullTitle
+  }
+},
+methods:{
+  submitEvent(newEvent){
+    if(newEvent==''){
+     
+      return this.error=true;
+    }
+    store.getActiveDay().events.push({
+      details: newEvent, edit: false 
+    })
+    this.error=false
+    this.newEvent=""
+
   }
 }
 
